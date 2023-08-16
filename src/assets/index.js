0aa3815b12002590430aa19c46581ce95d3862a8
Sora <sorazora1.4@gmail.com>
2023-08-16 04:56:35 -0700

    removed unused files and organized code

--- src/assets/index.js
+++ src/assets/index.js
@@ -29,7 +29,6 @@ import studying from './company/studying.png'
 
 import github from "./github.png";
 import logo from "./logo.svg";
-import creator from "./creator.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
@@ -40,7 +39,6 @@ import tripguide from "./tripguide.png";
 export {
   logo,
   backend,
-  creator,
   mobile,
   web,
   github,
