dc773c101940a8a5545250871bd5ba843c4e3f7d
Sora <sorazora1.4@gmail.com>
2023-08-16 04:53:16 -0700

    updated work experience section

--- src/assets/index.js
+++ src/assets/index.js
@@ -22,20 +22,16 @@ import git from "./tech/git.png";
 import figma from "./tech/figma.png";
 
 
-
-
+// Work Experience Logos
+import bcit from './company/bcit.png'
+import keg from './company/keg.jpg'
+import studying from './company/studying.png'
 
 import github from "./github.png";
 import logo from "./logo.svg";
 import creator from "./creator.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-
-
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -60,10 +56,6 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
   carrent,
   jobit,
   tripguide,
@@ -72,5 +64,8 @@ export {
   game,
   nextjs,
   prisma,
-  python
+  python,
+  bcit,
+  keg,
+  studying
 };
