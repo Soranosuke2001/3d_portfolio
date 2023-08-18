c05b3f8ec468827fe54beef3029697aacf1eb6fe
Sora <sorazora1.4@gmail.com>
2023-08-18 12:18:19 -0700

    functional download resume button

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -16,7 +16,9 @@ const Resume = () => {
         <button className="custom-button" onClick={() => setIsOpen(true)}>
           View Resume
         </button>
-        <button className="custom-button">Download Resume</button>
+        <a href={resume} download="Resume">
+          <button className="custom-button">Download Resume</button>
+        </a>
       </div>
       <Modal
         isOpen={modalIsOpen}
@@ -25,18 +27,13 @@ const Resume = () => {
         className="resume-modal"
       >
         <div className="absolute right-3 top-3">
-          <button
-            onClick={() => setIsOpen(false)}
-          >
+          <button onClick={() => setIsOpen(false)}>
             <XSquare className="h-16 w-16" />
           </button>
         </div>
         <div className="h-full flex justify-center items-center">
           <div className="h-[90%] w-5/6">
-            <iframe
-              src={resume}
-              className="h-full w-full"
-            ></iframe>
+            <iframe src={resume} className="h-full w-full"></iframe>
           </div>
         </div>
       </Modal>
