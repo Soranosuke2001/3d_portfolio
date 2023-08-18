0185d27c0f8982c6dbea76d18eb0d432ad6360d5
Sora <sorazora1.4@gmail.com>
2023-08-18 10:52:38 -0700

    added a basic modal

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import {
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary">
+      <div id="main" className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,12 +1,40 @@
 import React from "react";
+
 import { SectionWrapper } from "../hoc";
+import Modal from "react-modal";
+
+Modal.setAppElement("body");
 
 const Resume = () => {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
   return (
-    <div className="flex justify-around">
-      <button className="custom-button">View Resume</button>
-      <button className="custom-button">Download Resume</button>
-    </div>
+    <>
+      <div className="flex justify-around">
+        <button className="custom-button" onClick={() => setIsOpen(true)}>
+          View Resume
+        </button>
+        <button className="custom-button">Download Resume</button>
+      </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setIsOpen(false)}
+        contentLabel="Example Modal"
+        className="resume-modal"
+      >
+        <div className="flex flex-col justify-around h-full">
+          <button onClick={() => setIsOpen(false)}>close</button>
+          <div>I am a modal</div>
+          <form>
+            <input />
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+          </form>
+        </div>
+      </Modal>
+    </>
   );
 };
 
